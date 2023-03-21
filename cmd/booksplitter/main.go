f4d48249547769f71eec47c17f6112fdd35b98ce
Jeremy Gibson <jgibs1@gmail.com>
2023-03-21 14:27:22 -0400

    Recursive find of audiobooks added
    Scans a directory tree for any file ending with an m4b extension.

--- cmd/booksplitter/main.go
+++ cmd/booksplitter/main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"github.com/JeremyGibson/booksplitter/internal"
+	"io/fs"
 	"os"
+	"path/filepath"
 )
 
 func processSingleBook(path string) {
@@ -12,11 +14,24 @@ func processSingleBook(path string) {
 	ab.SetChapters()
 	ab.SetFormat()
 	ab.ExtractChapters()
-	fmt.Printf("Finished: %s/n", path)
+	fmt.Printf("Finished: %s\n\n\n", path)
 }
 
 func processMultipleBooks(rootDir string) {
-	fmt.Printf("Processing: %s/n", rootDir)
+	err := filepath.Walk(rootDir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+		if filepath.Ext(info.Name()) == ".m4b" {
+			processSingleBook(path)
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("error walking the path %q: %v\n", rootDir, err)
+		return
+	}
 }
 
 func main() {
