f51d92d2573690b1d1561dc8342915784a41434e
Jeremy Gibson <jgibs1@gmail.com>
2023-03-24 15:01:40 -0400

    Adds Date and Comment metadata

--- internal/audiofile.go
+++ internal/audiofile.go
@@ -4,10 +4,13 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/frolovo22/tag"
+	"github.com/schollz/progressbar/v3"
 	"image"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
+	"time"
 )
 
 const baseOutputPathAudio = "/data/audiofiles/extracted"
@@ -18,6 +21,7 @@ var coverImageBuffer = new(image.YCbCr)
 type AudioFileMeta struct {
 	FileInfo struct {
 		FilePath  string `json:"file_path"`
+		Source    string `json:"source"`
 		Album     string `json:"album"`
 		ImageTime string `json:"image_time"`
 		Artist    string `json:"artist"`
@@ -74,9 +78,14 @@ func (a *AudioFileMeta) setTrackMetadata(trackPath string, title string, trackNu
 	if err != nil {
 		fmt.Println(err)
 	}
+	year, err := strconv.Atoi(a.FileInfo.Year)
+	time, err := time.Parse("2006-01-02", a.FileInfo.Date)
 	tags.SetTitle(title)
 	tags.SetTrackNumber(trackNum, len(a.Tracks))
 	tags.SetArtist(a.FileInfo.Artist)
+	tags.SetYear(year)
+	tags.SetDate(time)
+	tags.SetDescription(a.FileInfo.Source)
 	tags.SetAlbumArtist(a.FileInfo.Artist)
 	tags.SetAlbum(a.FileInfo.Album)
 	tags.SetGenre(a.FileInfo.Genre)
@@ -86,12 +95,15 @@ func (a *AudioFileMeta) setTrackMetadata(trackPath string, title string, trackNu
 func (ae *AudioExtractor) ProcessAudioFile() {
 	ReadFrameAsJpeg(ae.AudioMeta.FileInfo.FilePath, ae.AudioMeta)
 	extractTo := ae.AudioMeta.setOutput()
+	pb := progressbar.Default(int64(len(ae.AudioMeta.Tracks)))
 	for num, track := range ae.AudioMeta.Tracks {
 		fileName := fmt.Sprintf("%03d_%s.flac", num+1, normalizeFileName(track.Title))
+		pb.Describe(fmt.Sprintf("Extracting: %s", fileName))
 		fmt.Printf("Extracting: %s\n", fileName)
 		outName := filepath.Join(extractTo, fileName)
 		cmd := "ffmpeg -i " + ae.AudioMeta.FileInfo.FilePath + " -ss " + track.FromSecs + " -to " + track.ToSecs + " -sample_fmt s16 -q:a 0 -map a " + outName + " -y"
 		executeCmd(cmd)
 		ae.AudioMeta.setTrackMetadata(outName, track.Title, num+1)
+		pb.Add(1)
 	}
 }
